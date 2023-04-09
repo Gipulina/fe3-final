ef303943ba7a2ddd75e9c626dd05f7e32f7bcdd5
gipulina90 <georgi_palavecino@hotmail.com>
2023-04-08 23:54:55 -0300

    cards with information and 4 per line

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { ContextGlobal } from "./utils/global.context";
 
 const Card = ({ name, username, id }) => {
@@ -11,13 +11,13 @@ const Card = ({ name, username, id }) => {
 
   return (
     <div className="card">
-      <img src="../images/doctor.jpg" alt="" />
-        {/* En cada card deberan mostrar en name - username y el id */}
-
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+        <img src="../images/doctor.jpg" alt="Doctor"/>
+        <h3>{name}</h3>
+        <h4>{username} </h4>
+          {/* En cada card deberan mostrar en name - username y el id */}
+          {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+          {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+          <button onClick={addFav} className="favButton">Add fav</button>
     </div>
   );
 };
